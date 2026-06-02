0e46dafdee9d0b7d23dcb9c34f55ad3778027b12
Gecko 51 <177301843+Gecko51@users.noreply.github.com>
2026-06-02 16:10:52 +0200

    Create function_input.py

---
+++ function_input.py
@@ -0,0 +1,4 @@
+utilisateur = input("Quel est votre prénom ? ")
+ville_de_naissance = input("Quelle est votre ville de naissance ? ")
+age_utilisateur = input("Quel est votre âge ? ")
+print(utilisateur + ", " + ville_de_naissance + ", " + age_utilisateur)
